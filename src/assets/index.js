26bd5ea7d1ac7a0fba35dfa0c1fac3c9afdb7d31
JosuerBague <83200271+JosuerBague@users.noreply.github.com>
2022-01-08 23:06:20 +0800

    Finished with footer skeleton.

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as ArrowLeft } from "./icon-arrow-left.svg";
+export { default as ArrowRight } from "./icon-arrow-right.svg";
+export { default as Facebook } from "./icon-facebook.svg";
+export { default as Instagram } from "./icon-instagram.svg";
+export { default as Location } from "./icon-location.svg";
+export { default as Twitter } from "./icon-twitter.svg";
